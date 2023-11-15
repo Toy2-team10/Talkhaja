6da5773f5e7d92431cd8786831a8f01fe6073592
developer-jyyun <developer.jyyun@gmail.com>
2023-11-15 13:11:35 +0900

    Feat: 마시막 메시지 n분 전/n 시간 전으로 변경

--- utils/timeFormat.ts
+++ utils/timeFormat.ts
@@ -11,11 +11,19 @@ export default function formatTime(updatedAt: Date | string): TimeFormatResult {
   if (timeDiffInSeconds < 60) {
     return { timeDiffText: '방금', className: 'time-now' };
   }
+  if (timeDiffInSeconds < 1800) {
+    // 30분 이내
+    const minutesAgo = Math.floor(timeDiffInSeconds / 60);
+    return { timeDiffText: `${minutesAgo}분 전`, className: 'time-minutes' };
+  }
   if (timeDiffInSeconds < 3600) {
     return { timeDiffText: '30분 전', className: 'time-minutes' };
   }
+
   if (timeDiffInSeconds < 86400) {
-    return { timeDiffText: '1시간 전', className: 'time-hours' };
+    // 1일
+    const hoursAgo = Math.floor(timeDiffInSeconds / 3600);
+    return { timeDiffText: `${hoursAgo}시간 전`, className: 'time-hours' };
   }
   const days = Math.floor(timeDiffInSeconds / 86400);
   return { timeDiffText: `${days}일 전`, className: 'time-days' };
